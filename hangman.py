4487ee58b814aa27a8c3ce634daed9373a7a11fc
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-03-27 20:13:27 -0400

    Draw the current state of hangman

--- hangman.py
+++ hangman.py
@@ -61,7 +61,39 @@ r"""
 CATEGORY = 'Animals'
 WORDS = 'ANT BABOON BADGER BAT BEAR BEAVER CAMEL CAT CLAM COBRA COUGAR COYOTE CROW DEER DOG DONKEY DUCK EAGLE FERRET FOX FROG GOAT GOOSE HAWK LION LIZARD LLAMA MOLE MONKEY MOOSE MOUSE MULE NEWT OTTER OWL PANDA PARROT PIGEON PYTHON RABBIT RAM RAT RAVEN RHINO SALMON SEAL SHARK SHEEP SKUNK SLOTH SNAKE SPIDER STORK SWAN TIGER TOAD TROUT TURKEY TURTLE WEASEL WHALE WOLF WOMBAT ZEBRA'.split()
 
+def drawHangman(missedLetters, correctLetters, secretWord):
+    '''Draw the current state of Hangman, along with the correctly guessed and missed letters of the secret word'''
+    print(HANGMAN_PICS[len(missedLetters)])
+    print('The category is: ', CATEGORY)
+    print()
+
+    # Show the incorrectly guessed letters:
+    print('Missed letters: ', end='')
+    for letter in missedLetters:
+        print(letter, end= ' ')
+    if len(missedLetters) == 0:
+        print('No missed letters yet.')
+    print()
+
+    # Display the blanks for the secret word (one blank per letter)
+    blanks = ['_'] * len(secretWord)
+
+    # Replaced blanks with correctly guessed letters
+    for i in range(len(secretWord)):
+        if secretWord[i] in correctLetters:
+            blanks[i] = secretWord[i]
+
+    # Show the secret word with spaces in between each letter
+    print(' '.join(blanks))
+
 def main():
+    # Set up variables for a new game
+    missedLetters = [] # List of incorrect letter guesses
+    correctLetters = [] # List of correct letter guesses
+    secretWord = random.choice(WORDS) # The word the player must guess
+
+    while True: # Main game loop
+        drawHangman(missedLetters, correctLetters, secretWord)
 
 if __name__ == "__main__":
     try:
